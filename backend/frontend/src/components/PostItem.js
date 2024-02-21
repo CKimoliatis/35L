718b1c3b90eb3028ff7020aa1befdacf54cb6515
Christian Escobar <chris_cobar@outlook.com>
2024-02-20 20:39:57 -0800

    swapped the content of Landing page and PostItem page to route them correctly

--- backend/frontend/src/components/PostItem.js
+++ backend/frontend/src/components/PostItem.js
@@ -1,13 +1,56 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
+import NavigationBar from "./NavigationBar";
+import "./UploadButton";
+import "../CSS/styles.css";
+import UploadButton from "./UploadButton";
 
 const PostItem = () => {
-    return (
-        <div>
-            <p>
-                This is the landing page
-            </p>
-        </div>
-    )
-}
+  return (
+    <>
+      <NavigationBar />
+      <br />
+      <br />
+      <br />
+      <div className="form-container">
+        <h2>Create a new listing</h2>
+        <form>
+          <div className="form-group">
+            <label htmlFor="title">Title</label>
+            <input type="text" id="title" placeholder="Add a title..." />
+          </div>
+          <div className="form-group">
+            <label htmlFor="price">Price</label>
+            <input type="text" id="price" placeholder="$" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="category">Category</label>
+            <select id="category">
+              <option disabled selected>
+                Select a category
+              </option>
+              <option>Clothing</option>
+              <option>Sports</option>
+              <option>Furniture</option>
+              <option>Other</option>
+            </select>
+          </div>
+          <div className="form-group">
+            <label htmlFor="description">Description</label>
+            <textarea
+              id="description"
+              placeholder="Describe your item"
+            ></textarea>
+          </div>
+          <div className="forum-group">
+            <label htmlFor="images">Upload pictures</label>
+            <br />
+            <UploadButton />
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </>
+  );
+};
 
-export default PostItem
+export default PostItem;

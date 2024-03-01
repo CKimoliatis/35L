910afd0dd86654a4f0695809542685d436c85a36
Ryan Carney <ryancarney771@gmail.com>
2024-03-01 12:47:25 -0800

    Added api call to landing page in order to fetch items from database to use in posts

---
+++ backend/frontend/src/components/ItemPage/ItemPage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import NavigationBar from '../NavigationBar'
+
+const ItemPage = () => {
+
+    return (
+    <>
+    <NavigationBar />
+    <br></br>
+    <br></br>
+    <br></br>
+    </>
+    );
+}
+
+export default ItemPage
--- backend/frontend/src/components/Posts/Post.js
+++ backend/frontend/src/components/Posts/Post.js
@@ -1,11 +1,12 @@
 import React from "react"
 
 
-function Post({image, description}) {
+function Post({image, price, title, description}) {
     return (
         <div className='post float-on-hover'>
             <img src={image} alt={description} className="post-images"></img>
-            <p className='description-text'>post description</p>
+            <h2>${price}</h2>
+            <p className='description-text'>{title}</p>
         </div>
     );
 }
